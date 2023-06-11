55f01bbc2d77a8b5ff30c1b4a3bd99fc1547b546
merlizkit <merlizkit@gmail.com>
2023-06-11 11:40:13 -0300

    Versión 2.1: creación y update con objeto

--- prueba.js
+++ prueba.js
@@ -1,11 +1,49 @@
-let product = {
-    code: 'Error',
-    title: 'Titulo',
-    description: 'Descripción',
-    price: 15,
-    thumbnail: 'Sin imagen',
-    stock: 500,
-};
+// let product = {
+//     code: 'Error',
+//     title: 'Titulo',
+//     description: 'Descripción',
+//     price: 15,
+//     thumbnail: 'Sin imagen',
+//     stock: 500,
+// };
 
-const checkData = product.filter(product => product === 'Error');
-console.log(checkData);
+// const checkData = product.filter(product => product === 'Error');
+// console.log(checkData);
+
+/* ----------------------------- spread operator ---------------------------- */
+
+const array = [ 1,2,3,5,6,7 ];
+console.log(Math.min(...array));
+
+const array2 = [ ...array, 10, 55, 210 ]
+
+console.log(array2);
+
+const obj1 = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+console.log(obj1);
+const obj3 = {
+    b: 99
+}
+console.log(obj3);
+
+const obj2 = {
+    ...obj1,
+    ...obj3,
+    d: 4
+}
+
+console.log(obj2);
+
+/* ------------------------------ rest operator ----------------------------- */
+
+const myFunc = (a, b, ...otherParamethers) => {
+    console.log(a);
+    console.log(b);
+    console.log(otherParamethers);
+}
+console.log('ejecutando function con operador rest:::');
+myFunc(1, 2, 3, 4, 5, 6, 'a', [1,2,3,4,5,5], {a: 1})
