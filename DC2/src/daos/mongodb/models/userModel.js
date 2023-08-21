102c3f2ccc626065d1f1b05a75fad935bfdfa530
merlizkit <merlizkit@gmail.com>
2023-08-21 14:09:51 -0300

    Cart id en user model

--- DC2/src/daos/mongodb/models/userModel.js
+++ DC2/src/daos/mongodb/models/userModel.js
@@ -22,6 +22,10 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    cartId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'products',
+        required: true },
     role: {
         type: String,
         default: 'user'
